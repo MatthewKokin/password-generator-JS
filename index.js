237768df6deb3b6e7a4e026426386bfe31964643
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-06-28 13:43:01 +0100

    Cleanned code with a help of GPT :)

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
-const Btn = document.getElementById("generate-btn")
-const firstPassword = document.getElementById("first")
-const secondPassword = document.getElementById("second")
-const ImputLength = document.getElementById("input-field")
+const generateBtn = document.getElementById("generate-btn");
+const firstPassword = document.getElementById("first");
+const secondPassword = document.getElementById("second");
+const inputLength = document.getElementById("input-field");
 
+generateBtn.addEventListener("click", function () {
+  const length = parseInt(inputLength.value);
+  firstPassword.textContent = generatePassword(length);
+  secondPassword.textContent = generatePassword(length);
+});
 
-
-Btn.addEventListener("click", function(){
-    firstPassword.textContent = password(ImputLength.value)
-    secondPassword.textContent = password(ImputLength.value)
-})
-
-
-
-// ----------------Functions------------
-function randomNumber() {
-    let number = Math.floor( Math.random() * characters.length)
-    return characters[number] 
+function getRandomCharacter() {
+  const randomIndex = Math.floor(Math.random() * characters.length);
+  return characters[randomIndex];
 }
 
-function password(length){
-    passwordCharacters = []
-    for (let i=0; i < length; i++){
-        let num = randomNumber()
-        passwordCharacters.push(num)
-    }
-    let password = passwordCharacters.join('');
-    return password
+function generatePassword(length) {
+  const passwordCharacters = [];
+  for (let i = 0; i < length; i++) {
+    const character = getRandomCharacter();
+    passwordCharacters.push(character);
+  }
+  const password = passwordCharacters.join("");
+  return password;
 }
 
-
-password(8)
-
-
+// Generate an initial password on page load
+inputLength.value = 8;
+firstPassword.textContent = generatePassword(8);
+secondPassword.textContent = generatePassword(8);
